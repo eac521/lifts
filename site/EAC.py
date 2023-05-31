fa60edec3dccc1a73967225532823a85e6875ad2
eac521 <50027931+eac521@users.noreply.github.com>
2023-05-31 15:21:41 -0400

    adding new directory for code base

---
+++ site/EAC.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+
+class training():
+    '''
+    This will be the class that houses the lifting data. So velocity history/show, Meso training log, meso creation, warm-up calc,
+    '''
+
+    def setup_meso(self):
+        '''
+        this will create the training template for the time set in the file
+        '''
+    def show_today(self):
+        '''
+        this will be the primary output for what is shown on the page
+        '''
+    def plot_velo(self):
+        '''
+        Show prior velocity training ressults
+        '''
+    def warmup(self):
+        '''
+        get warmup calculator to setup our warmup sets for the initial lift
+        '''
+
+class cardio ():
+    '''
+    This will track active steps and bike riding that is done.
+    '''
