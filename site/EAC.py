23b6a66670afb2b5e69d9be18f7993d046b29d67
eac521 <50027931+eac521@users.noreply.github.com>
2023-06-25 07:46:54 -0400

    added in new functions to read in csv to build out meso

--- site/EAC.py
+++ site/EAC.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
+import sqlite3
 
-class training():
+class training:
     '''
     This will be the class that houses the lifting data. So velocity history/show, Meso training log, meso creation, warm-up calc,
     '''         
@@ -20,15 +21,20 @@ class training():
         get warmup calculator to setup our warmup sets for the initial lift
         '''
 
-class cardio ():
+class cardio:
     '''
     This will track active steps and bike riding that is done.
     '''
 
-class database():
+class database:
+    '''
+    To be used to updated database information, will auto connect when instantiated
+
+    '''
     def __init__(self):
         conn = sqlite3.connect('training.db')
-    
-    def meso_load(self):    
-        files = [f for f in os.listdir('../data/uploads')]
-        
+
+    def update_program(self,rowData,connection):
+        for ct,row in enumerate(rowData):
+            str(ct)+row[0]
+
