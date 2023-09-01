bd069d5f5507f5bc3c60e1acf5ad2fffd79b03bc
eac521 <50027931+eac521@users.noreply.github.com>
2023-09-01 14:13:54 -0400

    update to database class, added in streamlit python file

--- site/EAC.py
+++ site/EAC.py
@@ -31,10 +31,57 @@ class database:
     To be used to updated database information, will auto connect when instantiated
 
     '''
-    def __init__(self):
-        conn = sqlite3.connect('training.db')
+    def __init__(self): 
+        self.conn = sqlite3.connect('training.db')
+        
+    def add_data(self,data,table):
+        with open(data,'r') as d:
+            for i,row in enumerate(d):
+                if i==0:
+                    continue
+                else:
+                    print(row)
+                    row = row.replace('\n','')
+                    qs = (len(row.split(','))-1)*'?,'+'?'
+                    self.conn.execute('''INSERT into {t} values ({row})'''.format(t=table,row=qs),row.split(','))
+            self.conn.commit()
+        return '{} has been updated with {} rows of data'.format(table,i)
+    
+    def create_meso_cycle(self,file):
+        df = pd.DataFrame(columns = ['movement','method','week','sets','reps','weight','notes','targetReps'])
+        prgbld = pd.read_sql('''
+        select * 
+        from programBuilder
+        ''',conn)
+        mesoDF = pd.read_csv(file).fillna(0)
+        v = df.columns[-5:].values
+        r=0
+        for sesh in mesoDF.session.unique():
+            d = mesoDF[mesoDF.session == sesh]
+            for movement in d.exercise.unique():
+                endweek = d[d.exercise==movement].endWeek.values[0]
+                mod = d[d.exercise==movement].modality.values[0]
+                cyclelength = prgbld[prgbld.method==mod].week.max()
+                setAdjust = d[d.exercise==movement].SetAdjust.values[0]
+                print(movement,mod,setAdjust)
+                for i in range(endweek):
+                    lkup = i+1 if cyclelength==endweek else (i+1) % cyclelength
+                    lkup = cyclelength if lkup == 0 else lkup
+                    l = [movement,mod,i+1]+prgbld[(prgbld.method==mod) & (prgbld.week==lkup)].filter(v).values.tolist()[0]
+                    df.loc[len(df)] = l
+                    df.at[r,'sets'] = df.at[r,'sets'] + setAdjust
+                    r+=1
+        with self.conn as connection:
+            add_data(df.values,'upcomingTraining')
 
-    def update_program(self,rowData,connection):
-        for ct,row in enumerate(rowData):
-            str(ct)+row[0]
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
 
---
+++ site/app_str.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+
+str.write("Test for the app!")
