b4a38de65c89120949bbac4346af2a28dd4fd2f8
eac521 <50027931+eac521@users.noreply.github.com>
2023-09-21 09:11:54 -0400

    new updates

--- site/app_str.py
+++ site/app_str.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import pandas as pd
+import sqlite3
 from EAC import site
-
+conn = sqlite3.connect('training.db')
 st.write("Test for the app!")
+st.write("Day:")
+st.selectbox('Choose Day:',)
+st.subheader("Primary Work:")
+train = pd.read_sql('''select * from
+upcommingTraining
+where ''',conn)
+st.subheader("Secondary Work:")
+
+st.dataframe(train)
