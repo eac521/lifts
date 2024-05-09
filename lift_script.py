efd83b1c0d441d638529c7b0549447fe5b7cf050
Eric Coxon <ericcoxon@macbook-air.mynetworksettings.com>
2024-05-09 16:09:37 -0400

    updated meso creation to comply with data format.  Updated the data update to actually work

--- lift_script.py
+++ lift_script.py
@@ -11,7 +11,11 @@ def create_meso_cycle(path,connection):
     select methodID, lower(method) as method, week, sets,reps,weight,notes,targetReps
     from programBuilder
     ''',conn)
-    mesoDF = pd.read_csv(path).fillna(0)
+    mesoDF = pd.read_csv(dataPath+trainingFile).fillna(0)
+    maxes = pd.read_sql('''
+    select movement,actualMax
+    from movements
+    ''',conn)
     v = df.columns[-5:].values
     r=0
     for sesh in mesoDF.session.unique():
@@ -28,9 +32,18 @@ def create_meso_cycle(path,connection):
                 df.loc[len(df)] = l
                 df.at[r,'sets'] = df.at[r,'sets'] + setAdjust
                 r+=1
+    final = df[['movement','method']].drop_duplicates().reset_index(drop=True)
+    final.movement = final.movement.str.lower()
+    final = final.merge(maxes,how='left',on='movement')
+    for wk in df.week.unique():
+        final['sets{}'.format(wk)] = df[df.week==wk].sets.values
+        final['reps{}'.format(wk)] = df[df.week==wk].reps.values
+        final['weight{}'.format(wk)] = df[df.week==wk].weight.values
+        final['targetReps{}'.format(wk)] = df[df.week==wk].targetReps.values
+    
     path = './site/initialData/meso_file.csv'
-    df.to_csv(path)
-    return df,path
+    final.to_csv(path)
+    return final,path
 def add_data(path,table,connection,insert=False):
     if insert:
         cols = pd.read_sql('select * from {} limit 1'.format(table),conn).columns
@@ -50,31 +63,41 @@ def add_data(path,table,connection,insert=False):
     connection.commit()
     print('{} has been updated with {} rows of data'.format(table,i))
     
+
 def update_movements_table(training_path,conn):
     '''
     Will take a csv of the training file, will find differences and update the database
     '''
-    training = pd.read_csv(training_path,skiprows=1)
+    training = pd.read_csv('./InitialData/training - TrainingCopy.csv',skiprows=1)
     training.movement = training.movement.str.lower()
     df = training.filter(['movement']+[col for col in training.columns if col.find('wrk')>-1])
     df = df.dropna().set_index('movement')
-    mvmts = '","'.join(df.index.values)
-    mvmt = pd.read_sql('''select * from movements where movement in ("{}")'''.format('","'.join(mvmts)),conn)
+    mvmts = '","'.join(df.index.str.lower().values)
+    mvmt = pd.read_sql('''select * from movements where movement in ("{}")'''.format(mvmts),conn)
     ct=0
     for lft in df.index.values:
+    
+        lft = lft.lower()
+        print(lft)
         try:
-            if lft not in mvmt.movement.values:
-                print('Adding in {} into movements table'.format(lft))
-                d = [lft] + [np.nan] * (len(mvmt.columns) - 1)
+            if lft not in mvmt.movement.str.lower().values:
+                print('\tAdding into movements table'.format(lft))
+                d = [lft,None,df.loc[lft]['wrk_10']] + [np.nan] * (len(mvmt.columns) - 3)
                 add_data(d,'movements',conn,True)
                 ct+=1
-            elif df.loc[lft]['wrk_10'] != int(mvmt[mvmt.movement=='convo']['actualMax'].values[0]):
-                conn.execute('''UPDATE movements
-                                    SET actualMax = {0} where movement = {1}'''.format(df.loc[lft]['wrk_10'],lft))
-                ct+=1
-            conn.commit()
+            else:
+                print('\tHave {} in database table'.format(lft))
         except:
-            print('error')
+            print('\t{} NOT ADDED'.format(lft.upper()))
+        try:
+            if df.loc[lft]['wrk_10'] != mvmt[mvmt.movement==lft]['actualMax'].values[0]:
+                print('\tNew working max')
+                conn.execute('''UPDATE movements
+                        SET actualMax = {0} where movement = "{1}" '''.format(df.loc[lft]['wrk_10'],lft))
+            ct+=1
+        except:
+            print('\tMax same as prior meso') 
+        conn.commit()
     print('updated movements table with {} edits'.format(ct))
 
 _,path,prgname = max([(os.stat(f)[-1],f.path,f.name) for f in os.scandir('./site/InitialData/') if re.search('PPL.csv$',f.name)!=None])
